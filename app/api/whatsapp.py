806ebaa74ab2249b3bfc5b9823cb1ee877fbf477
geekytamer <tamer.abbasher@gmail.com>
2025-05-19 00:59:23 +0800

    fix busses issue

--- app/api/whatsapp.py
+++ app/api/whatsapp.py
@@ -129,16 +129,13 @@ async def process_webhook(msg: dict, db: Session):
                     db.commit()
 
             case ConversationStateEnum.awaiting_bus:
-                buses = db.query(Bus).filter_by(accommodation_id=user.accommodation_id).all()
+                buses = db.query(Bus).filter_by(university_id=user.university_id).all()
                 selected = None
-                print(text)
                 if text.isdigit():
-                    print("is digit")
                     index = int(text) - 1
                     if 0 <= index < len(buses):
                         selected = buses[index]
                 else:
-                    print("is not digit")
                     for bus in buses:
                         if bus.name.lower() == text:
                             selected = bus
